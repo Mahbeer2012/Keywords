e9f812679524b958b99e8d28b95cb4605dab1850
Mahbeer2012 <mahbeermubashir031@gmail.com>
2025-03-15 14:18:12 +0500

    Activity1.py

---
+++ Activity1.py
@@ -0,0 +1,8 @@
+b = 10       #initaialise
+while b > 0:       #iterate loop
+    b = b - 1
+    if b == 5:    #condition 1
+        continue   #continue statement
+    #display result
+    print ('\ncurrent variable value :', b)
+print ("\ngood bye!")
